97c1d16dd009db92ab741532574a218cf385c5e5
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-02-10 13:24:05 -0600

    vinces pic and paragraph

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ function App() {
   return (
     <div className="App">
      <h1>About Us</h1>
+     <div>
+      <img src="./Vince.jpg"></img>
+      <p> Vince Hintermeister: Hailing from Tama Iowa Vince is an <br/>
+          avid art creator and a self-proclaimed super geek.</p>
     </div>
+     </div>
   );
 }
 
