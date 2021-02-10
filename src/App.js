65f2c44b5e992db37203abeb6ac7708eef44d54c
Kenny Jean <kennyjean561@gmail.com>
2021-02-10 14:16:24 -0500

    first commit kenny

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>About Us</h1>
+    <h1>About Us</h1>
+    <p>
+      My Name is Kenny,I have went sky-diving before
+    </p>
+    <img src={['https://www.skydivelongisland.com/images/article/is-skydiving-a-sport.jpg']} />
     </div>
   );
 }
